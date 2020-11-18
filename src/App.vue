002b435efba72b18a4f684687be24e33509243fe
YafiWebDev <yafiwebdev@gmail.com>
2020-11-18 22:16:06 +0200

    Load components and readd build.js

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
-	<div id="app">
+	<div>
 		<div class="font-bold text-center text-3xl text-gray-700 mt-24 mb-12">
             Simple Tailwind Todo App
         </div>
         <div class="max-w-2xl mx-auto border px-2 py-1 rounded shadow bg-gray-100">
             <input type="text" v-on:submit.prevent="addTask" placeholder="type a todo and press enter to save" class="w-full text-lg border-b mb-2">
             
+			<todo-items></todo-items>
+
         </div>
 	</div>
 </template>
 
 <script>
+import Items from './components/Items.vue';
+
 export default {
   	name: "app",
-	data() {
-		return {
-			msg: "Welcome to Your Vue.js App",
-		};
-	},
+	components: {
+		'todo-items': Items
+	}
 };
 </script>
--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,36 +1,47 @@
 <template>
 
-    <div v-for="(todo,id) in todos" :key="id" class="flex w-full space-between gap-4 text-lg items-center border-b">
-         items
-    </div>
+	<div>
+		<div v-for="(todo,id) in todos" :key="id" class="flex w-full space-between gap-4 text-lg items-center border-b">
+
+			<todo-item :todo="todo"></todo-item>
+
+		</div>
+	</div>
 
 </template>
 
 <script>
+	import Item from './Item.vue';
+
     export default{
-        data(){
-            todos: [
-                {
-                    title: "clean apartment",
-                    completed: false
-                },
-                {
-                    title: "get groceries",
-                    completed: false
-                },
-                {
-                    title: "cook dinner",
-                    completed: false
-                },
-                {
-                    title: "set plates",
-                    completed: false
-                },
-                {
-                    title: "think of dinner ideas",
-                    completed: true
-                },
-            ]
+		components: {
+			'todo-item': Item
+		},
+        data() {
+            return {
+				todos: [
+					{
+						title: "clean apartment",
+						completed: false
+					},
+					{
+						title: "get groceries",
+						completed: false
+					},
+					{
+						title: "cook dinner",
+						completed: false
+					},
+					{
+						title: "set plates",
+						completed: false
+					},
+					{
+						title: "think of dinner ideas",
+						completed: true
+					},
+				]
+			}
         }
     }
 </script>
