e78a918a8c8057d995758c5491ad58a5d281f55b
Christian von Uffel <christian@cvonu.com>
2020-11-20 16:30:12 -0500

    Create completed-tasks.vue

---
+++ src/components/completed-tasks.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-for="todo in todos" v-if="todo.completed" class="flex w-full space-between gap-6 text-lg items-center border-b">
+        <div class="bg-gray-400 hover:bg-gray-500 border border-gray-500 hover:border-gray-600 transition duration-150 h-8 w-8 m-2 rounded-full overflow-hidden cursor-pointer" v-on:click="toggleComplete">
+        </div>
+        <div v-else>
+            marked complete
+        </div>
+        <div class="w-full text-gray-500 line-through">
+            {{todo.title}}
+        </div>
+        <div v-on:click="removeTodo" class="text-gray-700 cursor-pointer p-1 px-2">✗</div>
+    </div>
+</template>
